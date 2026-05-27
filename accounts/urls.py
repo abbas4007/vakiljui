a4d0b24db6d1debfc78e0ac6c18ed2f337741d84
abbas4007 <abi.esmaeili@gmail.com>
2026-05-27 13:17:10 +0330

    third commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('ثبت-نام/', views.SignupView.as_view(), name='signup'),
-    path('ورود/', views.login_view, name='login'),
-    path('خروج/', views.logout_view, name='logout'),
+    re_path(r'^ثبت-نام/$', views.SignupView.as_view(), name='signup'),
+    re_path(r'^ورود/$', views.login_view, name='login'),
+    re_path(r'^خروج/$', views.logout_view, name='logout'),
 ]
