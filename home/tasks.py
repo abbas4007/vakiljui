8db65c0366597ca1c0c2b6da8563e12778fe50ae
abbas4007 <abi.esmaeili@gmail.com>
2026-06-23 13:07:08 +0330

    task update

--- home/tasks.py
+++ home/tasks.py
@@ -1,7 +1,20 @@
 from celery import shared_task
+from django.utils import timezone
+from datetime import timedelta
+from .models import LawyerSubscription
 
 
 @shared_task
-def hello():
-    print("Celery is working!")
-    return "ok"
+def check_expiring_subscriptions():
+
+    target_date = timezone.now() + timedelta(days=3)
+
+    subscriptions = LawyerSubscription.objects.filter(
+        is_paid=True,
+        end_date__date=target_date.date()
+    )
+
+    for sub in subscriptions:
+        print(
+            f"{sub.lawyer.user.email} expires soon"
+        )
