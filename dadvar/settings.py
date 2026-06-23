9ef99753288e79cec1efc16c21279dfa24b5b5d6
abbas4007 <abi.esmaeili@gmail.com>
2026-06-23 10:03:12 +0330

    Fix Celery Redis connection

--- dadvar/settings.py
+++ dadvar/settings.py
@@ -505,8 +505,15 @@ LOGGING = {
 # =========================================================
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
-CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_BROKER_URL = os.getenv(
+    "CELERY_BROKER_URL",
+    "redis://redis:6379/0"
+)
+
+CELERY_RESULT_BACKEND = os.getenv(
+    "CELERY_RESULT_BACKEND",
+    "redis://redis:6379/0"
+)
 
 CELERY_TIMEZONE = "Asia/Tehran"
 CELERY_ENABLE_UTC = False
